60fa7da404bf09a66d247e677aaceeac9a38b05c
ARAGAO67 <lucas.p.aragao2@gmail.com>
2026-08-31 14:26:50 +0000

    etapa de mapeamento de dados.csv

--- gestor_salas.py
+++ gestor_salas.py
@@ -7,4 +7,16 @@ startups = {
 projetos_ativos ={"App Mobile","Signal Found"}
 
 print("STARTUP:",startups["nome"], "Fase:",startups["etapa"])
-print("Projeto em Andamento:",projetos_ativos[0],"Inicio:",startups["inicio"])
+print("Projeto em Andamento:",list(projetos_ativos)[0],"Inicio:",startups["inicio"])
+
+#Etapa2 - Mapeamento de Salas
+#Sala Ocupada = 1 e Sala Livre =0
+salas = [
+    [1,0],
+    [0,1]
+]
+print("Status da Sala A1", salas[0][0])
+print("Status da Sala A2",salas[0][1])
+print("Status da Sala B2",salas [1][0])
+print("Status da Sala B2",salas[1][1])
+print("Classificação de Status: 1= Ocupado e 0 = Livre")
